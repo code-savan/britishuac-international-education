c3ea9bafdfc4e9fb2ac77ec19aef59587a4b03d2
code-savan <eric.marvelboy@gmail.com>
2026-07-20 16:50:33 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    trailingSlash: false,
+    trailingSlash: true,
     webpack: (config, { dev }) => {
         if (dev) {
             config.cache = false
